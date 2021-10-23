3f932fc1ce4ff92b7ad7ecdd06ea6746fccdeec3
hoshiki115 <hoshiki115@ybb.ne.jp>
2021-10-23 23:49:34 +0900

    sukkiriShopの追加

---
+++ sukkiriShop/src/main/java/test/LoginLogicTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import model.Login;
+import model.LoginLogic;
+
+public class LoginLogicTest {
+	public static void main(String[] args) {
+		testExecute1(); // ユーザーが見つかる場合のテスト
+		testExecute2(); // ユーザーが見つからない場合のテスト
+	}
+	public static void testExecute1() {
+		Login login = new Login("minato","1234");
+		LoginLogic bo = new LoginLogic();
+		boolean result = bo.execute(login);
+		if(result) {
+			System.out.println("testExecute1：成功しました");
+		} else {
+			System.out.println("testExecute1：失敗しました");
+		}
+	}
+	public static void testExecute2() {
+		Login login = new Login("minato","12345");
+		LoginLogic bo = new LoginLogic();
+		boolean result = bo.execute(login);
+		if(!result) {
+			System.out.println("testExecute2：成功しました");
+		} else {
+			System.out.println("testExecute2：失敗しました");
+		}
+	}
+}
